0d29e0c67ad072cc90331cea1d729befb6743ab6
Oleksandr_Mochakov <sasha.mochakov@ukr.net>
2023-06-13 15:38:45 +0300

    createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,43 +1,53 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import {
+  Route,
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
-import BlogPage from "./pages/BlogPage";
+import BlogPage, { blogLoader } from "./pages/BlogPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import Layout from "./components/Layout";
-import SinglePage from "./components/SinglePage";
+import SinglePage, { postLoader } from "./pages/SinglePage";
 import CreatePost from "./pages/CreatePost";
 import EditPost from "./pages/EditPost";
 import LoginPage from "./pages/LoginPage";
 import RequireAuth from "./hoc/RequireAuth";
 import AuthProvider from "./hoc/AuthProvider";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+      <Route path="about" element={<AboutPage />}>
+        <Route path="contacts" element={<p>Our contacts</p>} />
+        <Route path="team" element={<p>Our team</p>} />
+      </Route>
+      {/* переадресація задопомогою Navigate */}
+      <Route path="about-us" element={<Navigate to="/about" replace />} />
+      <Route path="posts" element={<BlogPage />} loader={blogLoader} />
+      <Route path="posts/:id" element={<SinglePage />} loader={postLoader} />
+      <Route path="posts/:id/edit" element={<EditPost />} />
+      <Route
+        path="posts/new"
+        element={
+          <RequireAuth>
+            <CreatePost />
+          </RequireAuth>
+        }
+      />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
+
 function App() {
   return (
     <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="about" element={<AboutPage />}>
-            <Route path="contacts" element={<p>Our contacts</p>} />
-            <Route path="team" element={<p>Our team</p>} />
-          </Route>
-          {/* переадресація задопомогою Navigate */}
-          <Route path="about-us" element={<Navigate to="/about" replace />} />
-          <Route path="posts" element={<BlogPage />} />
-          <Route path="posts/:id" element={<SinglePage />} />
-          <Route path="posts/:id/edit" element={<EditPost />} />
-          <Route
-            path="posts/new"
-            element={
-              <RequireAuth>
-                <CreatePost />
-              </RequireAuth>
-            }
-          />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+      <RouterProvider router={router} />
     </AuthProvider>
   );
 }
--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import {
+  Await,
+  Link,
+  defer,
+  useLoaderData,
+  useSearchParams,
+} from "react-router-dom";
 import BlogFilter from "../components/BlogFilter";
+import { Suspense } from "react";
 
 const BlogPage = () => {
-  const [posts, setPosts] = useState([]);
+  const { posts } = useLoaderData();
   const [searchParams, setSearchParams] = useSearchParams();
 
   const postQuery = searchParams.get("post") || "";
   const latest = searchParams.has("latest");
 
   const startsForm = latest ? 80 : 1;
-
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/posts`)
-      .then((res) => res.json())
-      .then((data) => setPosts(data));
-  }, []);
 
   return (
     <div className="container">
@@ -26,17 +26,38 @@ const BlogPage = () => {
         setSearchParams={setSearchParams}
       />
       <Link to="/posts/new">Add new post</Link>
-      {posts
-        .filter(
-          (post) => post.title.includes(postQuery) && post.id >= startsForm
-        )
-        .map((post) => (
-          <Link key={post.id} to={`${post.id}`}>
-            <li>{post.title}</li>
-          </Link>
-        ))}
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <Await resolve={posts}>
+          {(resolvedPosts) => (
+            <>
+              {resolvedPosts
+                .filter(
+                  (post) =>
+                    post.title.includes(postQuery) && post.id >= startsForm
+                )
+                .map((post) => (
+                  <Link key={post.id} to={`${post.id}`}>
+                    <li>{post.title}</li>
+                  </Link>
+                ))}
+            </>
+          )}
+        </Await>
+      </Suspense>
     </div>
   );
+};
+
+async function getPosts() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+  return res.json();
+}
+
+export const blogLoader = async () => {
+  return defer({
+    posts: getPosts(),
+  });
 };
 
 export default BlogPage;
